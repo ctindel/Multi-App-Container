d5546cc3535aafee680f01ff1fa6d37ef8cf963b
thoughtsociety <steve@thoughtsociety.org>
2018-08-23 07:50:49 -0400

    new app2 fixed pathname and app2

--- dash2/app2.py
+++ dash2/app2.py
@@ -15,7 +15,7 @@ import plotly.graph_objs as go
 import pandas as pd
 
 # Launch the application:
-app = dash.Dash(name='Bootstrap_docker_app',
+app2 = dash.Dash(name='Bootstrap_docker_app',
                 server=server,
                 url_base_pathname='/dash2/',
                 csrf_protect=False))
