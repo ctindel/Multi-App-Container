e0aaa9298e3792da49294e928e59250bf52f6501
thoughtsociety <steve@thoughtsociety.org>
2018-09-25 16:19:46 -0400

    added Flask imports

--- dash/app.py
+++ dash/app.py
@@ -1,7 +1,7 @@
 
 
 # simple stock comparitor
-
+from flask import Flask
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
---
+++ dash/import_test.py
@@ -0,0 +1,4 @@
+from requests import urllib3
+
+
+print('hello world')
